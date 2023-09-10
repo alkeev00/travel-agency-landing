fd7f20b115e590cb5490640184894c73076b4459
alkeev00 <alkeev2000@gmail.com>
2023-09-10 14:14:28 +0400

    add login form and signup form

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,52 @@
 import mobileNav from './modules/mobile-nav'
 mobileNav()
 
+document.querySelector('.btn-login').addEventListener('click', function () {
+	document.querySelector('.popup-login__wrapper').classList.add('active')
+})
+
+document
+	.querySelector('.popup-login__not-account')
+	.addEventListener('click', function () {
+		document.querySelector('.popup-login__wrapper').classList.remove('active')
+		document.querySelector('.popup-signup__wrapper').classList.add('active')
+	})
+
+document
+	.querySelector('.popup-login__close-btn')
+	.addEventListener('click', function () {
+		document.querySelector('.popup-login__wrapper').classList.remove('active')
+	})
+
+document
+	.querySelector('.popup-login__overlay')
+	.addEventListener('click', function () {
+		document.querySelector('.popup-login__wrapper').classList.remove('active')
+	})
+
+document.querySelector('.btn-signup').addEventListener('click', function () {
+	document.querySelector('.popup-signup__wrapper').classList.add('active')
+})
+
+document
+	.querySelector('.popup-signup__have-account')
+	.addEventListener('click', function () {
+		document.querySelector('.popup-signup__wrapper').classList.remove('active')
+		document.querySelector('.popup-login__wrapper').classList.add('active')
+	})
+
+document
+	.querySelector('.popup-signup__close-btn')
+	.addEventListener('click', function () {
+		document.querySelector('.popup-signup__wrapper').classList.remove('active')
+	})
+
+document
+	.querySelector('.popup-signup__overlay')
+	.addEventListener('click', function () {
+		document.querySelector('.popup-signup__wrapper').classList.remove('active')
+	})
+
 import Swiper from 'swiper/bundle'
 import 'swiper/css/bundle'
 
